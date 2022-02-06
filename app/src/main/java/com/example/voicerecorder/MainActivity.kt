b0d944ba4272e2bf3d93e0e7d00fa22c4ce30c7b
UlyanaSa <94441431+UlyanaSa@users.noreply.github.com>
2022-02-07 00:03:35 +0300

    Подключение меню в мейн активити, подключение вьюбайндинга, подключение в dependencies данных для room

--- app/src/main/java/com/example/voicerecorder/MainActivity.kt
+++ app/src/main/java/com/example/voicerecorder/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.voicerecorder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import com.example.voicerecorder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bind: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bind  = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bind.root)
+
+        NavigationUI.setupWithNavController(bind.bottomNavMenu,
+            Navigation.findNavController(this, R.id.nav_host_fragment_container))
     }
 }
